cac3aca22361729a18402f8a1ed819d07f666333
SkylarYHU <skylarhyn@gmail.com>
2024-10-20 23:30:43 +0100

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOST", "your-railway-project-url.railway.app")]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
